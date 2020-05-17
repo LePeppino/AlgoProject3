d5957b8cbf2f79cb61c6afadc4b8be8e31bd67ea
“LePeppino” <giuseppe.buccellato@hs-osnabrueck.de>
2020-05-17 10:33:56 +0200

    added classes Graph + DiGraph

---
+++ src/de/hsos/aud/project3/DiGraph.java
@@ -0,0 +1,4 @@
+package de.hsos.aud.project3;
+
+public class DiGraph {
+}
--- src/de/hsos/aud/project3/Main.java
+++ src/de/hsos/aud/project3/Main.java
@@ -3,6 +3,7 @@ package de.hsos.aud.project3;
 public class Main {
 
     public static void main(String[] args) {
-	// BENISBENISBENISBENISBENISBENISBENIS
+
+        System.out.println("Hello World!");
     }
 }
